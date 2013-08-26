9ffa21df71171d5cfb7c0b54b55c77781b8da300
Shengwei Li <senvey@gmail.com>
2013-08-25 22:41:57 -0700

    some dev notes while testing linkedin/facebook apis

--- main.js
+++ main.js
@@ -256,6 +256,23 @@ function load_weibo() {
   });
 }
 
+function load_linkedin() {
+	// token with full_profile and network access
+	var token = "AQXJsMV254lDeXNlmbbitcLtN-JRZbKK2g0fUp4qzJd53F6TVw21mckRTdHM-bMZVH5Lrutr7nnDbA6zDji1lHl70FK-GbjZy52xmCpK8ZiTL_kiQ0kD7Qx2pY0GR7-HUJd7d1OQa_KoWDoFmx_0ayzCcxbZryZHZxUuVKqasVdXQoz0ugA";
+	var profile_url = "https://api.linkedin.com/v1/people/~";
+	var basic_profile_fields = ["picture-url", "formatted-name", "headline", "location", "industry", "positions", "num-connections", "summary", "specialties", "public-profile-url"];
+	var full_profile_fields = ["skills", "educations", "last-modified-timestamp", "recommendations-received"];
+	var profile_filter = ":("+basic_profile_fields+","+full_profile_fields+")";
+	var url = profile_url+profile_filter+"?format=json&oauth2_access_token="+token;
+	
+	$.ajax({
+		url: "proxy.php?url="+encodeURIComponent(url),
+		dataType: "json",
+	}).done(function(data) {
+		console.log(data.formattedName);
+	})
+}
+
 function load_renren() {
   var renren_token = "240086|6.6ec70dc2deb5058696be9ce7fd21a57f.2592000.1379379600-282173283";
   var uid = "282173283";
@@ -295,6 +312,8 @@ function init() {
   load_book_list();
   load_movie_list();
   init_life();
+
+	load_linkedin();
 }
 
 init();
